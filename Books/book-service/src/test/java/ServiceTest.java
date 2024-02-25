bbd1886c829fe21b0616f25298e698a2d14849a0
董永燊 <471091869@qq.com>
2024-02-25 11:37:17 +0800

    add assert for uniTest

--- Books/book-service/src/test/java/ServiceTest.java
+++ Books/book-service/src/test/java/ServiceTest.java
@@ -1,17 +1,25 @@
 import com.vincent.BookApplication;
+import com.vincent.entity.Book;
 import com.vincent.service.BookService;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.util.Assert;
 
 @SpringBootTest(classes = BookApplication.class)
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@ContextConfiguration(classes = BookApplication.class)
 public class ServiceTest {
 
     @Resource private BookService service;
 
     @Test
     public void testGetBookById(){
-         service.getBookById(1);
+        Book book = service.getBookById(1);
+        Assert.isTrue(book.getBid() == 1,"");
     }
 
     @Test
